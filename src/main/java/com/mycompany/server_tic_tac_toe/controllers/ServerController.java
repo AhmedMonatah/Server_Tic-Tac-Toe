132fd128662b65b40a3886f485fe7f94012b5025
Andrew-Magdy-1 <andrewmagdy2142@gmail.com>
2025-12-18 20:48:19 +0200

    server ui is created

--- src/main/java/com/mycompany/server_tic_tac_toe/controllers/ServerController.java
+++ src/main/java/com/mycompany/server_tic_tac_toe/controllers/ServerController.java
@@ -2,12 +2,41 @@ package com.mycompany.server_tic_tac_toe.controllers;
 
 import com.mycompany.server_tic_tac_toe.App;
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 public class ServerController {
 
     @FXML
+    private Text num_Online;
+    @FXML
+    private Text num_Offline;
+    @FXML
+    private Text total_num;
+    @FXML
+    private Button stop_button;
+    @FXML
+    private Button start_button;
+    @FXML
+    private BarChart<?, ?> barChart;
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void stop_button_func(ActionEvent event) {
+    }
+
+    @FXML
+    private void start_button_func(ActionEvent event) {
+    }
+
+    @FXML
+    private void barChart(MouseEvent event) {
     }
 }
